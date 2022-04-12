16fc56c02b2a4a55ffd1e621e7871f1747f2649c
Richard Joyal <richardjoyal777@gmail.com>
2022-04-12 20:46:49 +0530

    Mongodb cloud done

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
-const config = require('config')
-const exprss = require('express');
-const app = exprss();
-const mongoose = require('mongoose');
-const genres = require('./routers/genres');
-const home = require('./routers/home');
-const customers = require('./routers/customer');
-const movies = require('./routers/movies')
-const rentals = require("./routers/rentals")
-const users = require('./routers/users');
-const auth = require('./routers/auth')
+const dotenv = require("dotenv");
+dotenv.config();
 
-if(!config.get('jwtPrivateKey')) {
-    console.error("FATAL ERROR: jwtPrivateKey is not defined")
-    process.exit(1);
+const config = require("config");
+const exprss = require("express");
+const app = exprss();
+const mongoose = require("mongoose");
+const genres = require("./routers/genres");
+const home = require("./routers/home");
+const customers = require("./routers/customer");
+const movies = require("./routers/movies");
+const rentals = require("./routers/rentals");
+const users = require("./routers/users");
+const auth = require("./routers/auth");
+
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined");
+  process.exit(1);
 }
 
 app.use(exprss.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers)
-app.use('/api/movies', movies)
-app.use('/api/rentals', rentals)
-app.use('/api/users', users)
-app.use('/api/auth', auth)
-app.use('/', home);
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/", home);
 
-mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log("Connected to mongodb"))
- .catch((err) => console.log("could not connect to mongodb", err));
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@vidly.l1jsu.mongodb.net/vidly?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connected to mongodb"))
+  .catch((err) => console.log("could not connect to mongodb", err));
+
+console.log(process.env.PORT);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Listening on Port ${port}`))
+app.listen(port, () => console.log(`Listening on Port ${port}`));
